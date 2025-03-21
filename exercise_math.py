486a9e71f2348ae6895d2764b3e1cbc59fce8c8c
LucasLeonard000 <lleonard@mail.austral.edu.ar>
2025-03-21 10:43:34 -0300

    Solved 3.0 exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,9 +1,9 @@
 def math():
-    f1 = 57
-    f2 = 7
-    print(f1 + f2)
-    print(f1 - f2)
-    print((f1+f2)/2)
-    print(f1//f2)
-    print(f1%f2)
-    print(f1/f2)
+    a = 57
+    b = 7
+    print(a + b)
+    print(a - b)
+    print((a+b)/2)
+    print(a//b)
+    print(a%b)
+    print(a/b)
